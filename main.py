7fed92aad29eb8b78e76f767adcf33238f125eb5
Юрченко Роман <ray1981@yandex.ru>
2022-08-01 16:09:41 +0300

    Готовая игра.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Программа-игра. Нужно угадать число, загаданное компьютером
+import random # импортируем модуль "Import"
+print ('Игра: угадай число, которое загадал компьютер')
+comp = random.randint(1,100) #Генерируем случайное целое число от 1 до 100 и помещаем его в переменную comp
+print ('Всё, я загадал число от 1 до 100. Посмотрим, с какой попытки Вы его угадаете! ))')
+print (comp) # Выводим число, загаданное компьютером (потом удалить, т.к. оно для разработчика)
+popitka = 1 # переменная для хранения кол-ва попыток
+user = 0 # переменная для ввода угадываемого числа
+while user != comp: # цикл: выполняется до тех пор, пока числа, загаданные компом и юзером, не совпадут
+    user = int (input ('Введите число: ')) # юзер вводит число
+    if user > comp:
+        print ('Ваше число больше, загаданного компьютером')
+        print ('Использована ', popitka, '-я попытка')
+        popitka = popitka + 1
+    elif user < comp:
+        print ('Ваше число меньше, загаданного компьютером')
+        print ('Использована ', popitka, '-я попытка')
+        popitka = popitka + 1
+    else: print ("Вы угадали это число с ", popitka, ' попытки! Кросаучек!')
