e33b490bfe0b2669f7736c5be767fb78d517e002
Юрченко Роман <ray1981@yandex.ru>
2022-08-02 14:18:51 +0300

    код программы "сортировка списка методом вставки"

--- Урок 2 - сортировка вектора (пузарьковый метод).py
+++
@@ -1,9 +0,0 @@
-from random import randint
-print('Программа сортировки массива 20-ти чисел, заполненными случайными числами от -20 до 20')
-numbers = []  # создаём массив numbers
-for i in range(20):
-    numbers.append(randint(-20, 20))
-
-
-
-print(numbers)
---
+++ Урок 2 - сортировка списка (сортировка вставкой).py
@@ -0,0 +1,12 @@
+from random import randint
+print('Программа сортировки списка из 20-ти чисел, заполненными случайными числами от -20 до 20')
+numbers = []  # создаём список numbers
+for i in range(20):  # цикл заполнения 20-ти элементов списка
+    numbers.append(randint(-20, 20))  # добавляем (append) случайные числа от -20 до 20 в numbers
+for i in range(1, len (numbers)):
+    while (i>0 and numbers[i]<numbers[i-1])^
+
+
+
+
+print(numbers)
